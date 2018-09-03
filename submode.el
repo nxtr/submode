2ab4f1485792922244df918912f738df70c57958
Magnus Nyberg <magnus@nexter.se>
2018-09-04 00:25:04 +0200

    Initial commit

---
+++ submode.el
@@ -0,0 +1,43 @@
+;;; submode.el --- Manage multiple active major modes  -*- lexical-binding: t -*-
+
+;; Copyright (C) 2018 Magnus Nyberg
+
+;; Author: Magnus Nyberg <magnus@nexter.se>
+;; Keywords: utilities, convenience, faces, languages, tools
+;; URL: https://github.com/nxtr/submode
+
+;; This file contains code from GNU Emacs, which is
+;; Copyright (C) 2017-2018 Free Software Foundation, Inc.
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Utilities for a major mode allowing multiple major modes to be active
+;; as submodes in one buffer.
+
+;; This library is inspired from the submode approach in mhtml-mode
+;; introduced in Emacs 26.1.
+
+;;; Code:
+
+(provide 'submode)
+
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; End:
+
+;;; submode.el ends here
